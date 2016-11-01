5f5593b2c5a51aa13c7f496d5b4d469c82611987
Agentscreech <travis.walentin@gmail.com>
2016-10-31 19:20:33 -0700

    fixed css to 100% width, added victory conditional

--- bonus-code/main.js
+++ bonus-code/main.js
@@ -1,14 +1,19 @@
 console.log("JS file is connected to HTML! Woo!");
-var cards = [];
+var cardPairs = [];
 var cardsInPlay = [];
 var cardsMatched = [];
 var toBeRemoved = [];
-var possibleCardsGenerator = function (qty) {
+var cards = [];
+var boardMaker = document.getElementById('game-board');
+
+var possibleCardsGenerator = function (qty) { // generates 2 arrays, one set of pairs for rendering and the other is for comparing matched cards for victory conditions
   for (var i = 1; i <= qty; i++){
-    cards.push(i,i);
+    cards.push(i);
+    cardPairs.push(i,i);
   }
 };
-function shuffle(array) { //stole this from stackOverflow
+
+function shuffle(array) { //stole this from stackOverflow, it randomizes the array.
   var currentIndex = array.length, temporaryValue, randomIndex;
 
   while (0 !== currentIndex) {
@@ -22,13 +27,12 @@ function shuffle(array) { //stole this from stackOverflow
   return array;
 }
 
-var boardMaker = document.getElementById('game-board');
 
 var createBoard = function(qty) {
     for (var i = 0; i < qty; i++){
     var newCard = document.createElement('div');
     newCard.className = "card";
-    newCard.setAttribute('data-card', cards[i]);
+    newCard.setAttribute('data-card', cardPairs[i]);
     newCard.addEventListener('click', isTwoCards);
     boardMaker.appendChild(newCard);
     }
@@ -39,21 +43,30 @@ var isTwoCards = function(){
     var label = this.getAttribute('data-card');
     this.innerHTML = '<p>' + label + '</p>';
     if (cardsInPlay.length === 2) {
-        isMatch(cardsInPlay);
-        cardsInPlay = [];
+        isMatch(cardsInPlay); // for some reason the timing of this isn't working like I thought it would. It's running the isMatch function before it's drawing the <p> tag
     }
 };
 
-
+var gameEnding = function () { // sorts the cards that have been matched from lowest to highest, then converts those from string to integer, then compares the 2 arrays.
+    cardsMatched.sort(function(a,b){return a - b});
+    cardsMatched=cardsMatched.map(Number);
+    var isEqual = (JSON.stringify(cards) === JSON.stringify(cardsMatched));
+    if (isEqual === true) {
+      alert("Congratulations! You matched them all!");
+    }
+};
+// TODO: get the <p> to clear just the unmatched carsd after no match. idea: for each cardsInPlay, for each cardPairs, if cardsInPlay = cardPairs[i], clear p tag.
 var clearUnmatched = function () {
-  for (var i = 0; i < cards.length; i++){
-    cards[i].innerHTML = "<p></p>";
+  for (var i = 0; i < cardPairs.length; i++){
+    cardPairs[i].innerHTML = "<p></p>";
   }
 };
+
 var isMatch = function() {
   if (cardsInPlay[0] === cardsInPlay[1]) {
     alert("You found a match!");
     cardsMatched.push(cardsInPlay[0]);
+    cardsInPlay = [];
     gameEnding();
   } else {
     clearUnmatched();
@@ -61,9 +74,9 @@ var isMatch = function() {
 };
 
 var startGame = function() {
-  var askQty = prompt("How many pairs do you want to use", "even numbers only");
+  var askQty = prompt("How many pairs do you want to use", "2 or more");
   possibleCardsGenerator(askQty);
-  cards = shuffle(cards);
+  cardPairs = shuffle(cardPairs);
   createBoard(askQty*2);
 };
 
